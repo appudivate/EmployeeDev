d2079fa2b3d85ae6f8ae9f6ce5ac586a82e2d317
Divate <2085120@cognizant.com>
2022-03-08 14:17:02 +0530

    my project

---
+++ DevopsAzure/Controllers/EmployeeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevopsAzure.Models;
+
+namespace DevopsAzure.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public List<Employee> GetEmployees()
+        {
+            List<Employee> empList = new List<Employee>()
+            {
+                new Employee() { EmployeeId = 1005, EmployeeFName = "Scott",Job = "Manager"},
+                new Employee() { EmployeeId = 1006, EmployeeFName = "Smith",Job = "Lead"},
+                new Employee() { EmployeeId = 1007, EmployeeFName = "Sandy",Job = "Programmerr"},
+                new Employee() { EmployeeId = 1008, EmployeeFName = "Sam",Job = "Lead"}
+            };
+
+            return empList;
+        }
+            [HttpGet("{id}")]
+
+            public Employee GetEmployeeById(int id)
+            {
+                List<Employee> empList = new List<Employee>()
+            {
+                new Employee() { EmployeeId = 1005, EmployeeFName = "Scott",Job = "Manager"},
+                new Employee() { EmployeeId = 1006, EmployeeFName = "Smith",Job = "Lead"},
+                new Employee() { EmployeeId = 1007, EmployeeFName = "Sandy",Job = "Programmerr"},
+                new Employee() { EmployeeId = 1008, EmployeeFName = "Sam",Job = "Lead"}
+            };
+                Employee obj = empList.SingleOrDefault(item => item.EmployeeId == id);
+                return obj;
+
+
+
+            }
+    }
+}
---
+++ DevopsAzure/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevopsAzure.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeFName { get; set; }
+
+        public string Job { get; set; }
+    }
+}
